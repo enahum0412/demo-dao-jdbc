b34ce7f5453b31b48da208947eed4f4f60e77ed2
Eduardo Nahum <edunahum@gmail.com>
2020-06-24 20:06:07 -0400

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in)
-				;
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -93,7 +93,7 @@ public class SellerDaoJDBC implements SellerDao {
 	public void deleteById(Integer id) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("DELETE FROM seller where Id = ?");
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 			
 			st.setInt(1, id);
 			
